a99d968ab9dc9300043bca5076a68aca95e854e1
fracabu <100199965+fracabu@users.noreply.github.com>
2022-07-13 10:14:34 +0200

    mod

--- index.php
+++ index.php
@@ -16,33 +16,20 @@ include 'database.php';
 
 <body>
     
-
     <main>
         <div class="container">
             <ul>
-
                 <?php foreach ($database as $cd) { ?>
-
                     <li>
                         <img src="<?php echo $cd["poster"]; ?>" alt="">
                         <h3><?php echo $cd["title"]; ?></h3>
                         <p><?php echo $cd["author"]; ?></p>
                         <small><?php echo $cd["year"]; ?></small>
                     </li>
-
-
-
-
                 <?php } ?>
-
             </ul>
-
         </div>
-
-
     </main>
-
-
 </body>
 
 </html>
